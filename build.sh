9c9f64287d4195d186b50740d0cc6658e4be2f9b
AlexanderR <aleksandr.rvachev@eltex.loc>
2019-11-27 16:10:03 +0700

    Remove call to go install taken from web guides

--- build.sh
+++ build.sh
@@ -26,7 +26,10 @@ GOARCH=amd64 go build -o parser_64.bin parser.go
 GOARCH=386 go build -o jtvgen_32.bin jtvgen.go
 GOARCH=amd64 go build -o jtvgen_64.bin jtvgen.go
 
-GOOS=windows GOARCH=386 go install
+# Online guide suggests to do this, but it does not work because
+# of https://github.com/golang/go/issues/11778 and other reasons
+#GOOS=windows GOARCH=386 go install
+
 GOOS=windows CGO_ENABLED=1 CXX=i686-w64-mingw32-g++ CC=i686-w64-mingw32-gcc \
  GOARCH=386 go build -o parser.exe parser.go
 GOOS=windows CGO_ENABLED=1 CXX=i686-w64-mingw32-g++ CC=i686-w64-mingw32-gcc \
