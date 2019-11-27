22baab046d2295110bd0a9a26a893253cbf12cf4
AlexanderR <aleksandr.rvachev@eltex.loc>
2019-11-27 16:00:33 +0700

    One more attempt to solve GOPATH glitch
    Now using a workaround from https://stackoverflow.com/a/32357023

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
-#!/bin/bash -e
-curl -O 'https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz'
+#!/bin/bash -ex
+wget -N 'https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz'
 tar xf 'go1.12.7.linux-amd64.tar.gz'
+
+mkdir -p bin src
 
 export GOROOT="`readlink -f ./go/`"
 export GOPATH="`readlink -f .`"
 export PATH="`readlink -f ./go/bin/`:$PATH"
+export GOBIN="$GOPATH/bin"
 
+export GIT_SSL_NO_VERIFY=1
 go get "golang.org/x/text/encoding"
 go get "golang.org/x/text/encoding/charmap"
 go get "golang.org/x/net/html/charset"
@@ -13,6 +17,8 @@ go get --insecure "gitlab.eltex.loc/aleksandr.rvachev/go-sqlite3.git"
 
 # required by sqlite
 export CGO_ENABLED=1
+
+mkdir -p bin src
 
 GOARCH=386 go build -o parser_32.bin parser.go
 GOARCH=amd64 go build -o parser_64.bin parser.go
