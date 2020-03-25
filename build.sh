fd4d3c215316ed922ae94ee53591ad3bcdfa7c11
AlexanderR <aleksandr.rvachev@eltex.loc>
2020-03-25 13:27:30 +0700

    Add version to application name

--- build.sh
+++ build.sh
@@ -26,23 +26,24 @@ export CGO_ENABLED=1
 
 mkdir -p bin src
 
-VER=1.0-$(git rev-parse --short HEAD)
+ELT_VER=1.0.0
+VER=$ELT_VER-$(git rev-parse --short HEAD)
 BUILDER="$(whoami)@$(hostname)"
 BUILD_TIME="$(date +'%d %B %Y %H:%M')"
 
 LDFLAGS=-ldflags="-X main.EltexPackageVersion=$VER -X \"main.EltexBuilder=$BUILDER\"  -X \"main.EltexBuildTime=$BUILD_TIME\""
 
-GOARCH=386 go build "$LDFLAGS" -o parser_32.bin parser.go
-GOARCH=amd64 go build "$LDFLAGS" -o parser_64.bin parser.go
+GOARCH=386 go build "$LDFLAGS" -o parser-$ELT_VER-x32.bin parser.go
+GOARCH=amd64 go build "$LDFLAGS" -o parser-$ELT_VER-x64.bin parser.go
 
-GOARCH=386 go build "$LDFLAGS" -o jtvgen_32.bin jtvgen.go
-GOARCH=amd64 go build "$LDFLAGS" -o jtvgen_64.bin jtvgen.go
+GOARCH=386 go build "$LDFLAGS" -o jtvgen-$ELT_VER-x32.bin jtvgen.go
+GOARCH=amd64 go build "$LDFLAGS" -o jtvgen-$ELT_VER-x64.bin jtvgen.go
 
 # Online guide suggests to do this, but it does not work because
 # of https://github.com/golang/go/issues/11778 and other reasons
 #GOOS=windows GOARCH=386 go install
 
 GOOS=windows CGO_ENABLED=1 CXX=i686-w64-mingw32-g++ CC=i686-w64-mingw32-gcc \
- GOARCH=386 go build "$LDFLAGS" -o parser.exe parser.go
+ GOARCH=386 go build "$LDFLAGS" -o parser-$ELT_VER.exe parser.go
 GOOS=windows CGO_ENABLED=1 CXX=i686-w64-mingw32-g++ CC=i686-w64-mingw32-gcc \
- GOARCH=386 go build "$LDFLAGS" -o jtvgen.exe jtvgen.go
+ GOARCH=386 go build "$LDFLAGS" -o jtvgen-$ELT_VER.exe jtvgen.go
